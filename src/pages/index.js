b0d3381f79b2dde2d63840526f674bc33981c1e3
github username <aslongman22@gmail.com>
2020-05-27 11:09:33 -0400

    counter start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React from "react";
+import Link from 'gatsby-link';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+    <div>
+      <h1>Bonjour!</h1>
+      <Link to='/counter/'>Counter</Link>
+    </div>
+  )
 }
