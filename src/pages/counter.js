2e9bf74541acf1ae0f5b34f1af7e5e0aefa4b27d
github username <aslongman22@gmail.com>
2020-05-27 11:20:39 -0400

    counter done

--- src/pages/counter.js
+++ src/pages/counter.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default class Counter extends React.Component{
+    state = {
+        count: 0
+    }
+
     render(){
         return(
-            <div>Counter</div>
+            <div>
+                <h1>Counter</h1>
+                <p>Current Count: {this.state.count}</p>
+                <button onClick={() => this.setState({count: this.state.count + 1})}>+</button>
+                <button onClick={() => this.setState({count: this.state.count - 1})}>-</button>
+            </div>
         )
     }
 }
